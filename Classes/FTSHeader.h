b610bacee65b46663215c381121450703bedd6b5
Anton <anton@Antons-iMac.local>
2018-11-20 01:35:14 -0800

    very minor fix

---
+++ Classes/FTSHeader.h
@@ -0,0 +1,17 @@
+//
+//  FTSHeader.h
+//  
+//
+//  Created by Anton on 11/12/18.
+//
+
+#ifndef FTSHeader_h
+#define FTSHeader_h
+
+#import "FTSBufsContainer.h"
+#import "FTSSearchParameters.h"
+#import "FTSController.h"
+#import "FTSQueryItem.h"
+#import "FTSItem.h"
+
+#endif /* FTSHeader_h */
